ed2624b3ae9fbb15d5d821d00ea585057811d156
Mahfuz <aamahfuz2@gmail.com>
2020-09-28 00:13:47 +0600

    fix footer issues

--- src/SortingVisualizer/Footer.js
+++ src/SortingVisualizer/Footer.js
@@ -5,19 +5,19 @@ function Footer() {
     return (
         <div>
             <footer class="bottom-container">
+                <p>Contact us</p>
                 <div className="upperFotter">
-                    <p>Contact us</p>
-                    <div className="contact"> 
+                    <div className="contact">
                         <p>Name: Akash Kumar Mondol</p>
                         <p>Email: akash.ice.ru@gmail.com</p>
                         <p>Mobile No: 01521-458326</p>
                     </div>
-                    <div className="contact"> 
+                    <div className="contact">
                         <p>Name: Abdullah Al Mahfuz</p>
                         <p>Email: aamahfuz2@gmail.com</p>
                         <p>Mobile No: 01718-896655</p>
                     </div>
-                    
+
                 </div>
                 <div class="footer-copyright text-center py-3"><p>© 2020 Copyright, Sorting Algorithm Visualizer, All right reserved. Developed by Akash and Mahfuz.</p></div>
             </footer>
